7da6e916d611b37a6977c469d18c03cf14aff236
Pratyayee Deb Purkayastha <pratyayeedev@gmail.com>
2021-10-24 17:38:14 +0530

    Styling the sign-in and sign-up pages

--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,45 @@
 //require express:
-const express=require('express');
-const router=express.Router();
+const express = require("express");
+const router = express.Router();
 
 //require passport:
-const passport=require('passport');
+const passport = require("passport");
 
-const usersController=require('../controllers/users_controller');
+const usersController = require("../controllers/users_controller");
+//const friendship_controller = require("../controllers/friendship_controller");
+router.get(
+  "/profile/:id",
+  passport.checkAuthentication,
+  usersController.profile
+);
+//router.get('/profile/:id/toggle_friend', friendship_controller.toggle_friendship);
+router.post(
+  "/update/:id",
+  passport.checkAuthentication,
+  usersController.update
+);
 
-router.get('/profile/:id',passport.checkAuthentication,usersController.profile);
-router.post('/update/:id',passport.checkAuthentication,usersController.update);
-
-router.get('/sign-up', usersController.signUp);
-router.get('/sign-in', usersController.signIn);
-router.post('/create',usersController.create);
+router.get("/sign-up", usersController.signUp);
+router.get("/sign-in", usersController.signIn);
+router.post("/create", usersController.create);
 
 //use passport as a middleware to authenticate:
-router.post('/create-session',
-    passport.authenticate('local',{failureRedirect:'/users/sign-in'},
-    ),
-usersController.createSession);
+router.post(
+  "/create-session",
+  passport.authenticate("local", { failureRedirect: "/users/sign-in" }),
+  usersController.createSession
+);
 
-router.get('/sign-out',usersController.destroySession);
+router.get("/sign-out", usersController.destroySession);
 
-router.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email']}));
-router.get('/auth/google/callback', passport.authenticate('google', {failureRedirect: '/users/sign-in'}), usersController.createSession);
+router.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+router.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/users/sign-in" }),
+  usersController.createSession
+);
 
-module.exports=router;
+module.exports = router;
