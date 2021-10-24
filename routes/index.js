d71a0bfc2526d1e6ed4bf72369d06d9de953bb5c
Pratyayee Deb Purkayastha <pratyayeedev@gmail.com>
2021-10-25 00:36:20 +0530

    add styles to the home page

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,20 @@
-const express=require('express');
-const router=express.Router();
+const express = require("express");
+const router = express.Router();
 
-const homeController=require('../controllers/home_controller');
+const homeController = require("../controllers/home_controller");
 
-console.log('router loaded');
+console.log("router loaded");
 
-router.get('/',homeController.home);
-router.use('/users',require('./users'));
-router.use('/posts',require('./posts'));
-router.use('/comments',require('./comments'));
-router.use('/likes', require('./likes'));
+router.get("/", homeController.home);
+router.use("/users", require("./users"));
+router.use("/posts", require("./posts"));
+router.use("/comments", require("./comments"));
+router.use("/likes", require("./likes"));
 
-router.use('/api', require('./api'));
+router.use("/api", require("./api"));
 
 //for any furthur routes access from here,
 //router.use('/routerName', require('./routerfile));
 
 //export it to index.js
-module.exports=router;
+module.exports = router;
